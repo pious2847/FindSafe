f45726c015e6f5050e8d664088eee1ffd8d6a608
pious <109950629+pious2847@users.noreply.github.com>
2024-04-30 20:35:45 +0000

    Changes to be committed: 	modified:   lib/screens/login.dart

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -30,6 +30,9 @@ class _SigninState extends State<Signin> {
         'password': user.password,
       },
     );
+    if(res.statuscode === 200 || res.statuscode === 201){
+         print(res.data);
+    }
     print(res.data);
     Navigator.push(
       context,
