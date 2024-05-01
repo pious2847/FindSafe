6691d50563247468707f688874d79801a957bfc0
pious <109950629+pious2847@users.noreply.github.com>
2024-05-01 07:09:39 +0000

    Changes to be committed: 	modified:   lib/screens/login.dart

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -53,151 +53,153 @@ class _SigninState extends State<Signin> {
         //       width: 400,
         //       height: 150,
         //     )),
-        Container(
-          alignment: Alignment.center,
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 150,
-                ),
-                Text(
-                  "Signin",
-                  style: GoogleFonts.pacifico(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 50,
-                      color: Colors.blue),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextFormField(
-                    controller: TextEditingController(text: user.email),
-                    onChanged: (value) {
-                      user.email = value;
-                    },
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Enter something';
-                      } else if (RegExp(
-                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                          .hasMatch(value)) {
-                        return null;
-                      } else {
-                        return 'Enter valid email';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        icon: const Icon(
-                          Icons.email,
-                          color: Colors.blue,
-                        ),
-                        hintText: 'Enter Email',
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.blue)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.blue)),
-                        errorBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.red)),
-                        focusedErrorBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.red))),
+        SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.center,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 150,
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextFormField(
-                    controller: TextEditingController(text: user.email),
-                    onChanged: (value) {
-                      user.email = value;
-                    },
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Enter something';
-                      }
-                      return null;
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        icon: const Icon(
-                          Icons.vpn_key,
-                          color: Colors.blue,
-                        ),
-                        hintText: 'Enter Password',
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.blue)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.blue)),
-                        errorBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.red)),
-                        focusedErrorBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.red))),
+                  Text(
+                    "Signin",
+                    style: GoogleFonts.pacifico(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 50,
+                        color: Colors.blue),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(55, 16, 16, 0),
-                  child: SizedBox(
-                    height: 50,
-                    width: 400,
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16.0),
-                        ),
-                      ),
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          save();
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      controller: TextEditingController(text: user.email),
+                      onChanged: (value) {
+                        user.email = value;
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter something';
+                        } else if (RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value)) {
+                          return null;
                         } else {
-                          print("not ok");
+                          return 'Enter valid email';
                         }
                       },
-                      child: const Text(
-                        "Signin",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      decoration: InputDecoration(
+                          icon: const Icon(
+                            Icons.email,
+                            color: Colors.blue,
+                          ),
+                          hintText: 'Enter Email',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(color: Colors.blue)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(color: Colors.blue)),
+                          errorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(color: Colors.red)),
+                          focusedErrorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(color: Colors.red))),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      controller: TextEditingController(text: user.email),
+                      onChanged: (value) {
+                        user.email = value;
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter something';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          icon: const Icon(
+                            Icons.vpn_key,
+                            color: Colors.blue,
+                          ),
+                          hintText: 'Enter Password',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(color: Colors.blue)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(color: Colors.blue)),
+                          errorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(color: Colors.red)),
+                          focusedErrorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(color: Colors.red))),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(55, 16, 16, 0),
+                    child: SizedBox(
+                      height: 50,
+                      width: 400,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            save();
+                          } else {
+                            print("not ok");
+                          }
+                        },
+                        child: const Text(
+                          "Signin",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                    padding: const EdgeInsets.fromLTRB(95, 20, 0, 0),
-                    child: Row(
-                      children: [
-                        const Text(
-                          "Not have Account ? ",
-                          style: TextStyle(
-                              color: Colors.black, fontWeight: FontWeight.bold),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const Signup()));
-                          },
-                          child: const Text(
-                            "Signup",
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(95, 20, 0, 0),
+                      child: Row(
+                        children: [
+                          const Text(
+                            "Not have Account ? ",
                             style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold),
+                                color: Colors.black, fontWeight: FontWeight.bold),
                           ),
-                        ),
-                      ],
-                    ))
-              ],
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Signup()));
+                            },
+                            child: const Text(
+                              "Signup",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ))
+                ],
+              ),
             ),
           ),
         )
