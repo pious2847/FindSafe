3eddc1000f897505fec643cc6a948ab82b87bc26
pious <109950629+pious2847@users.noreply.github.com>
2024-05-15 09:13:50 +0000

    Changes to be committed: 	deleted:    lib/utils/lost_mode_service.dart 	modified:   lib/utils/messages.dart

--- lib/utils/messages.dart
+++ lib/utils/messages.dart
@@ -1,4 +1,7 @@
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 class SnackbarUtils {
   static void showCustomSnackBar(BuildContext context, String message, Color backgroundColor) {
@@ -9,6 +12,19 @@ class SnackbarUtils {
         backgroundColor: backgroundColor,
         
       ),
+    );
+  }
+}
+
+
+class ToastMsg {
+  static void showToastMsg(BuildContext context, String message, Color backgroundColor) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      backgroundColor: backgroundColor,
+      textColor: Colors.white,
     );
   }
 }
