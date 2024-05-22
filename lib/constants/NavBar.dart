b471554d4daf75e343ab1646b6aeaf21d53830c3
pious <109950629+pious2847@users.noreply.github.com>
2024-05-22 11:36:49 +0000

    Changes to be committed: 	modified:   lib/constants/NavBar.dart

--- lib/constants/NavBar.dart
+++ lib/constants/NavBar.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: avoid_print, file_names, must_be_immutable
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lost_mode_app/.env.dart';
 import 'package:lost_mode_app/models/User_model.dart';
 import 'package:lost_mode_app/screens/locations.dart';
 import 'package:lost_mode_app/screens/login.dart';
@@ -12,15 +14,28 @@ import 'package:lost_mode_app/services/service.dart';
 import 'package:lost_mode_app/utils/messages.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 
-class NavBar extends StatelessWidget {
-   NavBar({super.key});
+class NavBar extends StatefulWidget {
+  NavBar({super.key});
 
-  UserProfileModel myUser = UserProfileModel(
-  username: 'John Doe',
-  email: 'john.doe@example.com',
-  phone: '+233 201025963',
-  password: 'password123',
-);
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  UserProfileModel? myUser;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUser();
+  }
+
+  // UserProfileModel myUser = UserProfileModel(
+  //   username: 'John Doe',
+  //   email: 'john.doe@example.com',
+  //   phone: '+233 201025963',
+  //   password: 'password123',
+  // );
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +45,8 @@ class NavBar extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: [
           UserAccountsDrawerHeader(
-            accountName: const Text("Abdul Hafis Mohammed"),
-            accountEmail: const Text('abdulhafis2847@gmail.com'),
+            accountName: Text(myUser?.username ?? "Loading..."),
+            accountEmail: Text(myUser?.email ?? "Loading..."),
             currentAccountPicture: CircleAvatar(
               child: ClipOval(
                 child: Image.asset('assets/images/avatar.jpg'),
@@ -50,33 +65,38 @@ class NavBar extends StatelessWidget {
                 Column(
                   children: [
                     ListTile(
-                        leading: const Icon(Iconsax.home_1_copy, color: Colors.purple,),
+                        leading: const Icon(
+                          Iconsax.home_1_copy,
+                          color: Colors.purple,
+                        ),
                         title: const Text('Dashboard'),
-                        onTap: () => {
-                          Get.to(const MapScreen())
-                        }),
-
+                        onTap: () => {Get.to(const MapScreen())}),
                     ListTile(
-                      leading: const Icon(Iconsax.user_octagon_copy,  color: Colors.purple),
+                      leading: const Icon(Iconsax.user_octagon_copy,
+                          color: Colors.purple),
                       title: const Text('Profile'),
                       onTap: () => {
-                        Get.to( UserProfile(user: myUser,))
+                        if (myUser != null)
+                          Get.to(UserProfile(user: myUser!))
+                        else
+                          print('User data not loaded yet'),
                       },
                     ),
-                                        ListTile(
-                      leading: const Icon(Iconsax.mobile_copy, color: Colors.purple),
+                    ListTile(
+                      leading:
+                          const Icon(Iconsax.mobile_copy, color: Colors.purple),
                       title: const Text('Devices'),
                       onTap: () => print('tapped'),
                     ),
                     ListTile(
-                      leading: const Icon(Iconsax.data_copy,  color: Colors.purple),
+                      leading:
+                          const Icon(Iconsax.data_copy, color: Colors.purple),
                       title: const Text('Locations'),
-                      onTap: () => {
-                        Get.to(const LocationHistory())
-                      },
+                      onTap: () => {Get.to(const LocationHistory())},
                     ),
                     ListTile(
-                      leading: const Icon(Iconsax.activity_copy,  color: Colors.purple),
+                      leading: const Icon(Iconsax.activity_copy,
+                          color: Colors.purple),
                       title: const Text('Modes'),
                       onTap: () => print('tapped'),
                     ),
@@ -85,18 +105,20 @@ class NavBar extends StatelessWidget {
                 Column(
                   children: [
                     ListTile(
-                      leading: const Icon(Iconsax.settings, color: Colors.purple),
+                      leading:
+                          const Icon(Iconsax.settings, color: Colors.purple),
                       title: const Text('Settings'),
-                      onTap: ()  async =>{
-                         Navigator.push(
+                      onTap: () async => {
+                        Navigator.push(
                           context,
                           MaterialPageRoute(
                               builder: (context) => const Settings()),
                         )
-                      } ,
+                      },
                     ),
                     ListTile(
-                      leading: const Icon(Iconsax.logout_1_copy, color: Colors.purple),
+                      leading: const Icon(Iconsax.logout_1_copy,
+                          color: Colors.purple),
                       title: const Text('Logout'),
                       onTap: () async => {
                         await logout(),
@@ -106,8 +128,11 @@ class NavBar extends StatelessWidget {
                           MaterialPageRoute(
                               builder: (context) => const Signin()),
                         ),
-                      // ignore: use_build_context_synchronously
-                      SnackbarUtils.showCustomSnackBar(context, 'Logout successful', const Color.fromARGB(255, 76, 175, 80)),
+                        // ignore: use_build_context_synchronously
+                        SnackbarUtils.showCustomSnackBar(
+                            context,
+                            'Logout successful',
+                            const Color.fromARGB(255, 76, 175, 80)),
                       },
                     ),
                   ],
@@ -120,5 +145,30 @@ class NavBar extends StatelessWidget {
     );
   }
 
+  Future<void> fetchUser() async {
+    try {
+      final userData = await getUserDataFromLocalStorage();
+      final userId = userData['userId'];
+      final dio = Dio();
+      final url = '$APIURL/api/get-user/$userId';
 
+      final response = await dio.get(
+        url,
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        print("Success Retrieve User data ${response.data}");
+        final userJson = response.data['User'];
+        setState(() {
+          myUser = UserProfileModel.fromJson(userJson);
+        });
+      }
+    } catch (e) {
+      print('An Error Occurred $e');
+    }
+  }
 }
