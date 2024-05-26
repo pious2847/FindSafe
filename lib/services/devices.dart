dfcc6b65181b8fd39eca147e567174dcbc5e39b1
pious <109950629+pious2847@users.noreply.github.com>
2024-05-26 17:24:44 +0000

    Changes to be committed: 	modified:   lib/services/devices.dart

--- lib/services/devices.dart
+++ lib/services/devices.dart
@@ -4,25 +4,24 @@ import 'package:lost_mode_app/models/devices.dart';
 import 'package:lost_mode_app/models/location_model.dart';
 
 class ApiService {
-  Future<List<Device>> fetchDevices(String userId) async {
-    final dio = Dio();
-    try {
-      final response =
-          await dio.get('${Uri.parse(APIURL)}/mobiledevices/$userId');
-      print('Response of devices: ${response.data}');
-      if (response.statusCode == 200) {
-        // Since response.data is already decoded, use it directly
-        List jsonResponse = response.data['mobileDevices'];
-        print(jsonResponse);
-        return jsonResponse.map((device) => Device.fromJson(device)).toList();
-      } else {
-        throw Exception('Failed to load devices');
-      }
-    } catch (error) {
-      print('Error fetching devices: $error');
+ Future<List<Device>> fetchDevices(String userId) async {
+  final dio = Dio();
+  try {
+    final response = await dio.get('${Uri.parse(APIURL)}/mobiledevices/$userId');
+    print('Response of devices: ${response.data}');
+    if (response.statusCode == 200) {
+      // Since response.data is already decoded, use it directly
+      List jsonResponse = response.data['mobileDevices'];
+      print(jsonResponse);
+      return jsonResponse.map((device) => Device.fromJson(device)).toList();
+    } else {
       throw Exception('Failed to load devices');
     }
+  } catch (error) {
+    print('Error fetching devices: $error');
+    throw Exception('Failed to load devices');
   }
+}
 
   Future<List<Location>> fetchLocationHistory(String deviceId) async {
     try {
