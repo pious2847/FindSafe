26e23c29aea610c6cb6de1075e437f602d7c8d81
pious <109950629+pious2847@users.noreply.github.com>
2024-05-29 12:10:40 +0000

    Changes to be committed: 	modified:   lib/screens/login.dart 	modified:   pubspec.yaml

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -12,6 +12,7 @@ import 'package:lost_mode_app/services/service.dart';
 import 'package:lost_mode_app/utils/messages.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:get/get.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class Signin extends StatefulWidget {
   const Signin({super.key});
@@ -26,10 +27,13 @@ class _SigninState extends State<Signin> {
   bool _obscureText = true;
   final emailController = TextEditingController();
   final passwordController = TextEditingController(); 
-  BuildContext? _dialogContext;
+   bool _isLoading = false; // Add this line
 
   Future<void> save() async {
-      _showLoadingDialog(); // Show the loading dialog
+    setState(() {
+    _isLoading = true; // Set loading state to true
+  });
+  
     final dio = Dio();
     try {
       final response = await dio.post(
@@ -77,9 +81,7 @@ class _SigninState extends State<Signin> {
           const Color.fromARGB(255, 76, 175, 80),
         );
        // Dismiss the loading dialog after successful login
-      if (_dialogContext != null) {
-        Navigator.of(_dialogContext!).pop();
-      }
+      
       } else {
         print("Invalid response ${response.statusCode}: ${response.data}");
       }
@@ -101,219 +103,223 @@ class _SigninState extends State<Signin> {
     '',
   );
   
- void _showLoadingDialog() {
-  showDialog(
-    barrierDismissible: false,
-    context: context,
-    builder: (BuildContext context) {
-      _dialogContext = context; // Store the dialog context
-      return AlertDialog(
-        content: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            CircularProgressIndicator(),
-            SizedBox(width: 16),
-            Text("Loading..."),
-          ],
-        ),
-      );
-    },
-  );
-}
+//  void _showLoadingDialog() {
+//   showDialog(
+//     barrierDismissible: false,
+//     context: context,
+//     builder: (BuildContext context) {
+//       _dialogContext = context; // Store the dialog context
+//       return AlertDialog(
+//         content: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: const [
+//             CircularProgressIndicator(),
+//             SizedBox(width: 16),
+//             Text("Loading..."),
+//           ],
+//         ),
+//       );
+//     },
+//   );
+// }
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      children: [
-        // Positioned(
-        //     top: 0,
-        //     child: SvgPicture.asset(
-        //       'images/top.svg',
-        //       width: 400,
-        //       height: 150,
-        //     )),
-        SingleChildScrollView(
-          child: Container(
-            alignment: Alignment.center,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 150,
-                  ),
-                  Text(
-                    "Signin",
-                    style: GoogleFonts.pacifico(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 50,
-                        color: Colors.purple),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextFormField(
-                      controller: TextEditingController(text: user.email),
-                      onChanged: (value) {
-                        user.email = value;
-                      },
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Field cannot be empty.';
-                        } else if (RegExp(
-                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                            .hasMatch(value)) {
-                          return null;
-                        } else {
-                          return 'Enter valid email';
-                        }
-                      },
-                      decoration: InputDecoration(
-                          hintText: 'Enter Email',
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide:
-                                  const BorderSide(color: Colors.purple)),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide:
-                                  const BorderSide(color: Colors.purple)),
-                          errorBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide: const BorderSide(color: Colors.red)),
-                          focusedErrorBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide: const BorderSide(color: Colors.red))),
+    return ModalProgressHUD(
+       inAsyncCall: _isLoading,
+      child: Scaffold(
+          body: Stack(
+        children: [
+          // Positioned(
+          //     top: 0,
+          //     child: SvgPicture.asset(
+          //       'images/top.svg',
+          //       width: 400,
+          //       height: 150,
+          //     )),
+          SingleChildScrollView(
+            child: Container(
+              alignment: Alignment.center,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 150,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextFormField(
-                      controller: TextEditingController(text: user.password),
-                      onChanged: (value) {
-                        user.password = value;
-                      },
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Field cannot be empty.';
-                        }
-                        return null;
-                      },
-                      obscureText: _obscureText,
-                      decoration: InputDecoration(
-                          hintText: 'Enter Password',
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _obscureText
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _obscureText = !_obscureText;
-                              });
-                            },
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide:
-                                  const BorderSide(color: Colors.purple)),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide:
-                                  const BorderSide(color: Colors.purple)),
-                          errorBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide: const BorderSide(color: Colors.red)),
-                          focusedErrorBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide: const BorderSide(color: Colors.red))),
+                    Text(
+                      "Signin",
+                      style: GoogleFonts.pacifico(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 50,
+                          color: Colors.purple),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(16.0, 16, 16, 0),
-                    child: SizedBox(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width,
-                      child: TextButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.resolveWith<Color>(
-                            (Set<MaterialState> states) {
-                              if (states.contains(MaterialState.disabled)) {
-                                return Colors.grey; // Disabled button color
-                              }
-                              return Get.isDarkMode
-                                  ? Colors.purple[700]!
-                                  : Colors
-                                      .purple; // Button color based on theme
-                            },
-                          ),
-                          shape: MaterialStateProperty.all<OutlinedBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16.0),
-                            ),
-                          ),
-                        ),
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            _showLoadingDialog(); // Show the loading dialog
-                            await save(); // Wait for the save function to complete
-                           
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        controller: TextEditingController(text: user.email),
+                        onChanged: (value) {
+                          user.email = value;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Field cannot be empty.';
+                          } else if (RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value)) {
+                            return null;
                           } else {
-                            print("not ok");
+                            return 'Enter valid email';
                           }
                         },
-                        child: const Text(
-                          "Signin",
-                          style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            hintText: 'Enter Email',
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                borderSide:
+                                    const BorderSide(color: Colors.purple)),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                borderSide:
+                                    const BorderSide(color: Colors.purple)),
+                            errorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                borderSide: const BorderSide(color: Colors.red)),
+                            focusedErrorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                borderSide: const BorderSide(color: Colors.red))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        controller: TextEditingController(text: user.password),
+                        onChanged: (value) {
+                          user.password = value;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Field cannot be empty.';
+                          }
+                          return null;
+                        },
+                        obscureText: _obscureText,
+                        decoration: InputDecoration(
+                            hintText: 'Enter Password',
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscureText
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
+                              },
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                borderSide:
+                                    const BorderSide(color: Colors.purple)),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                borderSide:
+                                    const BorderSide(color: Colors.purple)),
+                            errorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                borderSide: const BorderSide(color: Colors.red)),
+                            focusedErrorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                borderSide: const BorderSide(color: Colors.red))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16.0, 16, 16, 0),
+                      child: SizedBox(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width,
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.resolveWith<Color>(
+                              (Set<MaterialState> states) {
+                                if (states.contains(MaterialState.disabled)) {
+                                  return Colors.grey; // Disabled button color
+                                }
+                                return Get.isDarkMode
+                                    ? Colors.purple[700]!
+                                    : Colors
+                                        .purple; // Button color based on theme
+                              },
+                            ),
+                            shape: MaterialStateProperty.all<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16.0),
+                              ),
+                            ),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              _showLoadingDialog(); // Show the loading dialog
+                              await save(); // Wait for the save function to complete
+                             
+                            } else {
+                              print("not ok");
+                            }
+                          },
+                          child: const Text(
+                            "Signin",
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(95, 20, 0, 0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "Not have Account ? ",
-                          style: TextStyle(
-                            color: Get.isDarkMode ? Colors.white : Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const Signup(),
-                              ),
-                            );
-                          },
-                          child: Text(
-                            "Signup",
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(95, 20, 0, 0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Not have Account ? ",
                             style: TextStyle(
-                              color: Get.isDarkMode
-                                  ? Colors.purple[300]
-                                  : Colors.purple,
+                              color: Get.isDarkMode ? Colors.white : Colors.black,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ),
-                      ],
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Signup(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "Signup",
+                              style: TextStyle(
+                                color: Get.isDarkMode
+                                    ? Colors.purple[300]
+                                    : Colors.purple,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        )
-      ],
-    ));
+          )
+        ],
+      )),
+    );
   }
 }
