e6a2f6654533b3267c0e3f7dc68193cbcd2a8d79
pious <109950629+pious2847@users.noreply.github.com>
2024-05-29 12:16:31 +0000

    Changes to be committed: 	modified:   lib/screens/signup.dart

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously, avoid_print
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:lost_mode_app/.env.dart';
