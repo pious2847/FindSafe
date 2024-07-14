7bd7fd2280581c0fd026a00d80d7ce2a50de62e0
pious <109950629+pious2847@users.noreply.github.com>
2024-07-14 14:37:47 +0000

    Changes to be committed: 	modified:   lib/screens/profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -6,6 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:lost_mode_app/.env.dart';
 import 'package:lost_mode_app/models/User_model.dart';
 import 'package:lost_mode_app/services/service.dart';
@@ -40,189 +41,147 @@ class _UserProfileState extends State<UserProfile> {
           style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
         ),
       ),
-      body: CustomScrollView(
-        
-        slivers: [
-          Stack(
-                fit: StackFit.expand,
-                children: [
-                  Image.asset(
-                    'assets/images/avatar.jpg',
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: FittedBox(
                     fit: BoxFit.cover,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Colors.black.withOpacity(0.7),
-                        ],
+                    child: ImageFiltered(
+                      imageFilter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                      child: Image.asset(
+                        'assets/images/avatar.jpg',
+                        // height: 50,
                       ),
                     ),
                   ),
-                ],
-              ),
-         SliverToBoxAdapter(
-         child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Personal Info',
-                    style: GoogleFonts.poppins(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).primaryColor,
+                ),
+                const Positioned(
+                  bottom: -1,
+                  left: 20,
+                  child: FractionalTranslation(
+                    translation: Offset(0.1, -0.1),
+                    child: CircleAvatar(
+                      radius: 70,
+                      backgroundImage: AssetImage('assets/images/avatar.jpg'),
                     ),
                   ),
-                  const SizedBox(height: 16),
-                  _buildInfoCard('Email', _user.email, Icons.email),
-                  _buildInfoCard('Phone', _user.phone ?? '', Icons.phone),
-                  _buildInfoCard('Area', _user.addressInfo!.area ?? '', Icons.location_on),
-                  _buildInfoCard('House No', _user.addressInfo!.houseNo ?? '', Icons.home),
-                  _buildInfoCard('Emergency Name', _user.emergencyContact!.name ?? '', Icons.person),
-                  _buildInfoCard('Emergency Contact', _user.emergencyContact!.contact ?? '', Icons.contact_phone),
-                ],
+                ),
+              ],
+            ),
+            
+            const Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                "Personal Info:",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-         ),
-          // EditableCards(),
-        ],
-      ),
-    );
-  }
-
-  Expanded EditableCards() {
-    return Expanded(
-      child: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: const Text(
-                'Username',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(_user.username),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () => _showEditDialog('username', _user.username),
-              ),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          Card(
-            child: ListTile(
-              title: const Text(
-                'Email',
-                style: TextStyle(fontWeight: FontWeight.bold),
+            ExpansionTile(
+              title: Text(
+                _user.username,
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w200,
+                  fontSize: 17,
+                ),
               ),
-              subtitle: Text(_user.email),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () => _showEditDialog('email', _user.email),
+              subtitle: Text(
+                _user.email,
+                style: GoogleFonts.poppins(color: Colors.grey, fontSize:12),
               ),
+              children: [
+              EditableCards(),
+              ],
             ),
-          ),
-          Card(
-            child: ListTile(
-              title: const Text(
-                'Phone',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(_user.phone ?? ''),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () => _showEditDialog('phone', _user.phone ?? ''),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: const Text(
-                'Area',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(_user.addressInfo!.area ?? ''),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () =>
-                    _showEditDialog('area', _user.addressInfo!.area ?? ''),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: const Text(
-                'House No',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(_user.addressInfo!.houseNo ?? ''),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () => _showEditDialog(
-                    'houseNo', _user.addressInfo!.houseNo ?? ''),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: const Text(
-                'Emergency  Name',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(_user.emergencyContact!.name ?? ''),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () => _showEditDialog(
-                    'emergencyName', _user.emergencyContact!.name ?? ''),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: const Text(
-                'Emergency Contact',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(_user.emergencyContact!.contact ?? ''),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () => _showEditDialog(
-                    'emergencyContact', _user.emergencyContact!.contact ?? ''),
-              ),
-            ),
-          ),
-          // Add more ListTiles for other fields as needed
-        ],
-      ),
-    );
-  }
-
-  Widget _buildInfoCard(String title, String value, IconData icon) {
-    return Card(
-      elevation: 2,
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: ListTile(
-        leading: Icon(icon, color: Theme.of(context).primaryColor),
-        title: Text(
-          title,
-          style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text(
-          value,
-          style: GoogleFonts.poppins(),
-        ),
-        trailing: IconButton(
-          icon: const Icon(Icons.edit, color: Colors.grey),
-          onPressed: () => _showEditDialog(title.toLowerCase(), value),
+          ],
         ),
       ),
     );
   }
+ Widget EditableCards() {
+    return Column(
+      children: <Widget>[
+        _buildEditableListTile(
+          icon: Iconsax.user,
+          title: 'Username',
+          value: _user.username,
+          onEdit: () => _showEditDialog('username', _user.username),
+        ),
+        _buildEditableListTile(
+          icon: Iconsax.sms,
+          title: 'Email',
+          value: _user.email,
+          onEdit: () => _showEditDialog('email', _user.email),
+        ),
+        _buildEditableListTile(
+          icon: Iconsax.call,
+          title: 'Phone',
+          value: _user.phone ?? '',
+          onEdit: () => _showEditDialog('phone', _user.phone ?? ''),
+        ),
+        _buildEditableListTile(
+          icon: Iconsax.location,
+          title: 'Area',
+          value: _user.addressInfo!.area ?? '',
+          onEdit: () => _showEditDialog('area', _user.addressInfo!.area ?? ''),
+        ),
+        _buildEditableListTile(
+          icon: Iconsax.home,
+          title: 'House No',
+          value: _user.addressInfo!.houseNo ?? '',
+          onEdit: () => _showEditDialog('houseNo', _user.addressInfo!.houseNo ?? ''),
+        ),
+        _buildEditableListTile(
+          icon: Iconsax.user,
+          title: 'Emergency Name',
+          value: _user.emergencyContact!.name ?? '',
+          onEdit: () => _showEditDialog('emergencyName', _user.emergencyContact!.name ?? ''),
+        ),
+        _buildEditableListTile(
+          icon: Iconsax.call,
+          title: 'Emergency Contact',
+          value: _user.emergencyContact!.contact ?? '',
+          onEdit: () => _showEditDialog('emergencyContact', _user.emergencyContact!.contact ?? ''),
+        ),
+      ],
+    );
+  }
 
+  Widget _buildEditableListTile({
+    required IconData icon,
+    required String title,
+    required String value,
+    required VoidCallback onEdit,
+  }) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.purple,
+),
+      title: Text(
+        title,
+        style: GoogleFonts.poppins(fontWeight: FontWeight.w700),
+      ),
+      subtitle: Text(
+        value,
+        style: GoogleFonts.poppins(),
+      ),
+      trailing: IconButton(
+        icon: Icon(Iconsax.edit_copy, size: 16, ),
+        onPressed: onEdit,
+      ),
+    );
+  }
   Future<void> _updateUserData(String fieldName, String newValue) async {
     final userData = await getUserDataFromLocalStorage();
     final userId = userData['userId'] as String?;
@@ -243,8 +202,8 @@ class _UserProfileState extends State<UserProfile> {
 
     if (response.statusCode == 200) {
       final resmsg = response.data['message'];
-      SnackbarUtils.showCustomSnackBar(
-          context, '$resmsg', const Color.fromARGB(255, 76, 175, 80));
+      SnackbarUtils.showCustomSnackBar(context, '$resmsg',
+          const Color.fromARGB(255, 76, 175, 80));
 
       setState(() {
         switch (fieldName) {
@@ -308,29 +267,19 @@ class _UserProfileState extends State<UserProfile> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Edit $fieldName',
-              style: GoogleFonts.poppins(fontWeight: FontWeight.bold)),
+          title: Text('Edit $fieldName'),
           content: TextField(
             controller: _editingController,
-            decoration: InputDecoration(
-              hintText: 'Enter new $fieldName',
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
-            ),
+            decoration: InputDecoration(hintText: 'Enter new $fieldName'),
           ),
           actions: <Widget>[
             TextButton(
               onPressed: () => Navigator.pop(context),
-              child: Text('Cancel', style: GoogleFonts.poppins()),
+              child: const Text('Cancel'),
             ),
-            ElevatedButton(
+            TextButton(
               onPressed: () => Navigator.pop(context, _editingController.text),
-              child: Text('Save', style: GoogleFonts.poppins()),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).primaryColor,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)),
-              ),
+              child: const Text('Save'),
             ),
           ],
         );
